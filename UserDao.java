e895e587131c23cc4c530eb84c3cadbeef0e3e5f
Kimhannah <skqkdldhf98@naver.com>
2024-08-03 15:58:23 +0900

    refactor: DB Connection 중복 코드 추출

--- UserDao.java
+++ UserDao.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 
 public class UserDao {
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book1234");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
@@ -22,8 +21,7 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book1234");
+		Connection c = getConnection();
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 
@@ -39,6 +37,12 @@ public class UserDao {
 		c.close();
 
 		return user;
+	}
+
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book1234");
+		return c;
 	}
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
