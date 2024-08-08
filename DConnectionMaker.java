e3024b1d01e8df21f8e6b4e22b353a072ebe2775
Kimhannah <skqkdldhf98@naver.com>
2024-08-08 17:15:08 +0900

    refactor: 인터페이스 추상화로 connection 클래스에 종속된 문제 개선

---
+++ DConnectionMaker.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        // Connection 생성하는 코드
+        return null;
+    }
+}
--- UserDao.java
+++ UserDao.java
@@ -4,14 +4,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class UserDao {
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker;
 
     public UserDao() {
-        simpleConnectionMaker = new SimpleConnectionMaker();
+        // 하지만, 여전히 구체 클래스가 사용된다.
+        connectionMaker = new DConnectionMaker();
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -25,7 +26,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
