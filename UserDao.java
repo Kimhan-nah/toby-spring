1dbe166d6a51b5cd57d3336df8bdc61668a7b762
hannkim <skqkdldhf98@naver.com>
2024-08-08 17:02:21 +0900

    [1장 오브젝트와 의존관계] 1.3 DAO의 확장 (#4)
    refactor: 관심사 분리를 위한 클래스 생성

--- UserDao.java
+++ UserDao.java
@@ -4,8 +4,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class UserDao {
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao() {
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -19,7 +25,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
