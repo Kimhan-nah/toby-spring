5626e4bf830305e66ed86467f647437076859589
Kimhannah <skqkdldhf98@naver.com>
2024-08-09 15:08:44 +0900

    refactor: DaoFactory로 테스트와 의존 관계 설정에 대한 역할과 책임 분리

--- user/dao/UserDaoTest.java
+++ user/dao/UserDaoTest.java
@@ -5,15 +5,11 @@ import java.sql.SQLException;
 import user.domain.User;
 
 /**
- * 관계 설정 책임이 추가된 user.dao.UserDao 클라이언트인 main 메소드
+ * user.dao.UserDao 클라이언트인 main 메소드
  */
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        // UserDao가 사용할 user.dao.ConnectionMaker 구현 클래스 user.dao.DConnectionMaker
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        // DConnectionMaker와 user.dao.UserDao "오브젝트 사이의" 의존관계 설정
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("hannkim1");
