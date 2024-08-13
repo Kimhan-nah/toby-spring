63aca905e9b1347d505d75fc5cb2f74e228819a8
Kimhannah <skqkdldhf98@naver.com>
2024-08-13 17:12:56 +0900

    feat: DaoFactory를 Configuration Metadata(설정 정보)로 설정
    Java-based configuration metadata

--- user/dao/DaoFactory.java
+++ user/dao/DaoFactory.java
@@ -1,9 +1,14 @@
 package user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * UserDao의 생성 책임을 맡은 팩토리 클래스 (의존 관계 설정)
  */
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
@@ -13,6 +18,7 @@ public class DaoFactory {
     // }
 
     // dao가 많아질 경우 중복된 코드를 분리하기 위함이다.
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
