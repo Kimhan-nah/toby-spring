7330ec9c55cc1457c5b3a2b3969c6363ea1b606a
Kimhannah <skqkdldhf98@naver.com>
2024-08-13 17:14:07 +0900

    feat: ApplicationContext 생성하여 Bean 객체 생성

--- user/dao/UserDaoTest.java
+++ user/dao/UserDaoTest.java
@@ -2,6 +2,9 @@ package user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import user.domain.User;
 
 /**
@@ -9,10 +12,13 @@ import user.domain.User;
  */
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        // generic method 방식 사용하여 두번째 파라미터에 리턴 타입(UserDao.class) 지정
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("hannkim1");
+        user.setId("hannkim3");
         user.setName("김한나");
         user.setPassword("genius");
 
